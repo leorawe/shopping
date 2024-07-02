7ce72994b7ee83953fbc32e00c94ed26862274a6
Leora Wenger <leorawenger@gmail.com>
2024-07-02 13:42:38 -0400

    Delete old shopping list.

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,6 @@ import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import Item from './Item'
 
-const shoppingItems = [
-  {
-    id: 'a',
-    name: 'pepper',
-  },
-  {
-    id: 'b',
-    name: 'broccoli',
-  },
-  {
-    id: 'c',
-    name: 'onions',
-  },
-  {
-    id: 'd',
-    name: 'cream',
-  },
-]
-
 const fruitsVegetables =
   [
     { category: "Fruits", price: "1", name: "Apple", id: 1 },
@@ -40,24 +21,8 @@ const watermelon = {
 }
 
 function App() {
-
-  const [shopData, setShopData] = useState(shoppingItems);
-  const [name, setName] = React.useState('');
   const [item, setItem] = useState(watermelon);
   const [produce, setProduce] = useState(fruitsVegetables);
-
-
-  function handleChange(event) {
-    setName(event.target.value);
-  }
-
-  function handleAdd() {
-    // add item
-    const newList = shopData.concat({ name, price, category, id: uuidv4() });
-    const sortedNewList = newList.sort((a, b) => (a.name || "").localeCompare(b.name || ""));
-    setShopData(sortedNewList);
-  }
-
   const set = name => {
     return ({ target: { value } }) => {
       setItem(oldValues => ({ ...oldValues, [name]: value }));
@@ -81,16 +46,12 @@ function App() {
     }
   }
 
-  // Ensuring the key isn't a falsy value, or has null or undefined type.
-  const sortedItems = shopData.sort((a, b) => (a.name || "").localeCompare(b.name || ""));
-  // const sortedProduce = produce.sort((a, b) => (a.name || "").localeCompare(b.name || ""));
 
   useEffect(() => {
     setProduce[{ ...produce }];
     let newProd = produce.concat(item);
     setProduce(newProd);
-    setShopData(sortedItems);
-  }, [sortedItems]);
+  }, []);
 
   return (
     <div className="App">
@@ -99,18 +60,6 @@ function App() {
           Shopping List
         </h1>
       </header>
-      <div>
-        <input type="text" onChange={handleChange} />
-        <button type="button" onClick={handleAdd}>
-          Add
-        </button>
-      </div>
-      <ul>
-        {shopData.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-      <hr />
       <div className="formSection">
         <form onSubmit={onSubmit}>
           <label>
